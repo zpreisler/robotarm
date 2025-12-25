7e2eb0ec20e036245158684d3c74affcacee56ea
Zdenek Preisler <z.preisler@gmail.com>
2025-12-25 16:15:32 +0100

    all files

---
+++ frontend/vite.config.ts
@@ -0,0 +1,20 @@
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import devtools from 'solid-devtools/vite';
+
+export default defineConfig({
+  plugins: [devtools(), solidPlugin(), tailwindcss()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
+  },
+  build: {
+    target: 'esnext',
+  },
+});
